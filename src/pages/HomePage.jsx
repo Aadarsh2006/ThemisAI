e7d51a45e78b064a74ed6cd2efc22205497f3ffc
Aadarsh Jha <aadarshjha84@gmail.com>
2025-10-01 19:20:00 +0530

    File path issue fixed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,48 +1,19 @@
-import React, { useState } from 'react';
-import HeroSection from '../components/home/HeroSection';
-import ServicesGrid from '../components/home/ServicesGrid';
-import Testimonials from '../components/home/Testimonials';
-import UpcomingServices from '../components/home/UpcomingServices';
-import Footer from '../components/layout/Footer';
-import DocGenModal from '../components/features/DocGenModal';
-import LetterDrafterModal from '../components/features/LetterDrafterModal';
-import CaseStatusModal from '../components/features/CaseStatusModal';
-import CrimeConsequencesModal from '../components/features/CrimeConsequencesModal';
-import RightsExplainerModal from '../components/features/RightsExplainerModal';
-import HearingPrepModal from '../components/features/HearingPrepModal';
+import React from 'react';
+import HeroSection from 'components/home/HeroSection';
+import ServicesGrid from 'components/home/ServicesGrid';
+import Testimonials from 'components/home/Testimonials';
+import UpcomingServices from 'components/home/UpcomingServices';
 
-
-const HomePage = () => {
-  const [activeModal, setActiveModal] = useState(null);
-
-  const openModal = (modalId) => setActiveModal(modalId);
-  const closeModal = () => setActiveModal(null);
-
-  // State for hero section modals
-  const [showRedModal, setShowRedModal] = useState(false);
-  const [showBlueModal, setShowBlueModal] = useState(false);
-
+const HomePage = ({ onServiceSelect }) => {
   return (
-    <>
-      <HeroSection 
-        showRedModal={showRedModal}
-        setShowRedModal={setShowRedModal}
-        showBlueModal={showBlueModal}
-        setShowBlueModal={setShowBlueModal}
-      />
-      <ServicesGrid openModal={openModal} />
+    <div className="flex-grow">
+      <HeroSection onGetStarted={() => document.getElementById('services-grid')?.scrollIntoView({ behavior: 'smooth' })} />
+      <div id="services-grid">
+        <ServicesGrid onServiceSelect={onServiceSelect} />
+      </div>
       <Testimonials />
       <UpcomingServices />
-      <Footer />
-
-      {/* Modals for ServicesGrid */}
-      <DocGenModal show={activeModal === 'docGen'} onClose={closeModal} />
-      <LetterDrafterModal show={activeModal === 'letterDrafter'} onClose={closeModal} />
-      <CaseStatusModal show={activeModal === 'caseStatus'} onClose={closeModal} />
-      <CrimeConsequencesModal show={activeModal === 'crime'} onClose={closeModal} />
-      <RightsExplainerModal show={activeModal === 'rightsExplainer'} onClose={closeModal} />
-      <HearingPrepModal show={activeModal === 'hearingPrep'} onClose={closeModal} />
-    </>
+    </div>
   );
 };
 
